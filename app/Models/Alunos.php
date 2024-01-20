2314bdcc1bb92c10f9f2b82a1e164f54a5c36434
Mario-Coxe <cox3site59@gmail.com>
2024-01-20 12:28:06 +0100

    feat(Alunos): create model and migartion

---
+++ app/Models/Alunos.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Alunos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'class_id', 'is_active', 'sex', 'address', 'email', 'phone_number', 'password', 'photo'];
+
+    protected $casts = [
+        'password' => 'hashed',
+    ];
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function turmas(): BelongsTo
+    {
+        return $this->belongsTo(Turmas::class);
+    }
+}
