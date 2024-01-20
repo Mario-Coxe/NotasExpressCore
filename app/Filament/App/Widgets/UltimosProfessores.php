24f8291fe9fdc7c96b6453f5fb100cad3c61ce51
Mário Coxe <mario.coxe@ideiasdinamicas.com>
2024-01-20 23:56:42 +0000

    feat(ultimos professores): added possibility of seeing the last teacher

--- app/Filament/App/Widgets/UltimosProfessores.php
+++ app/Filament/App/Widgets/UltimosProfessores.php
@@ -2,23 +2,23 @@
 
 namespace App\Filament\App\Widgets;
 
-use App\Models\Alunos;
+use App\Models\Professores;
+use Filament\Facades\Filament;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
-use Filament\Facades\Filament;
 
-class UltimosAlunos extends BaseWidget
+class UltimosProfessores extends BaseWidget
 {
     protected static ?int $sort = 3;
     public function table(Table $table): Table
     {
         return $table
-            ->query(Alunos::query()->whereBelongsTo(Filament::getTenant()))
+            ->query(Professores::query()->whereBelongsTo(Filament::getTenant()))
             ->defaultSort('created_at', 'desc')
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
-                Tables\Columns\TextColumn::make('turmas.name'),
+                Tables\Columns\TextColumn::make('address'),
             ]);
     }
 }
